d27556f35659d6028449ad50dbc9b310ba0c78fc
Vahid Moosavi <vm@vm.local>
2024-10-14 10:30:27 +0200

    20241014

--- maputils.py
+++ maputils.py
@@ -16,40 +16,64 @@ def render_land_map(gdf,border_dgf,sel_cols_for_map,geom='polygon'):
 	if len(gdf)>0:
 		m = leafmapF.Map(measure_control=False,draw_control=False)
 		url = 'https://wms.geo.admin.ch/?'
-		m.add_wms_layer(url,'ch.swisstopo.swissimage',name="swisstopo satellite",maxZoom= 26,opacity= [1,0.5],attribution='geoadmin')
-		m.add_wms_layer(url,'ch.kantone.cadastralwebmap-farbe',name="land parcels",minZoom=18,maxZoom= 26,opacity= 0.45,shown=True,attribution='geoadmin')
+		m.add_wms_layer(url,'ch.swisstopo.swissimage',name="swisstopo satellite",maxZoom= None,opacity= [1,0.5],attribution='geoadmin')
+		m.add_wms_layer(url,'ch.kantone.cadastralwebmap-farbe',name="cadastral",minZoom=16,maxZoom= None,opacity= 0.45,shown=True,attribution='geoadmin')
 		
 		style_dict_border = {
-                    "stroke": True,
-                    "color": '#fd9869',
-                    "weight": .7,
-                    "opacity": 1,
-                    "fill": True,
-                    "fillColor": "#fd9869",
-                    "fillOpacity": 0.1,
-                    # "dashArray": "9"
-                    # "clickable": True,
-                }
+					"stroke": True,
+					"color": '#fd9869',
+					"weight": .7,
+					"opacity": 1,
+					"fill": True,
+					"fillColor": "#fd9869",
+					"fillOpacity": 0.1,
+					# "dashArray": "9"
+					# "clickable": True,
+				}
 		
 		style_dict_land = {
-                    "stroke": True,
-                    "color": '#892881',
-                    "weight": 4,
-                    "opacity": 1,
-                    "fill": True,
-                    "fillColor": "#892881",
-                    "fillOpacity": 0.4,
-                    # "dashArray": "9"
-                    # "clickable": True,
-                }
+					"stroke": True,
+					"color": '#892881',
+					"weight": 4,
+					"opacity": 1,
+					"fill": True,
+					"fillColor": "#892881",
+					"fillOpacity": 0.4,
+					# "dashArray": "9"
+					# "clickable": True,
+				}
 
 		if geom=='polygon':
-			m.add_gdf(border_dgf[['geometry']],info_mode='None',style = style_dict_border)
-			m.add_gdf(gdf[sel_cols_for_map],info_mode='on_click',style = style_dict_land)
-			bounds = border_dgf.total_bounds
-			m.zoom_to_bounds(bounds)
-			m.to_streamlit(height=700)
+			m.add_gdf(border_dgf[['geometry']],layer_name='gemeinde',info_mode='None',style = style_dict_border)
+			m.add_gdf(gdf[sel_cols_for_map],info_mode='on_click',layer_name='parzellen',style = style_dict_land)
+			# if 'parcel_table' in st.session_state:
+			# 	edited_rows = st.session_state.parcel_table['edited_rows']
+			# 	selected_row_ids = list([k for k in edited_rows.keys() if edited_rows[k]['selected']==True])
+			# else:
+			# 	selected_row_ids = []
 
+			selected_row_ids = []
+
+			if len(selected_row_ids)>0:
+				bounds = gdf.iloc[selected_row_ids].total_bounds
+			else:
+				bounds = border_dgf.total_bounds
+				
+			
+			# m.zoom_to_bounds(bounds)
+			
+			m.fit_bounds(bounds=[[bounds[1], bounds[0]], [bounds[3], bounds[2]]],
+				max_zoom=18
+				)
+			mp_center = ((bounds[1]+bounds[3])/2,(bounds[0]+bounds[2])/2)
+			from streamlit_folium import st_folium
+			if (len(selected_row_ids)>0) and ((len(selected_row_ids)<2)):
+				print('h')
+				st_folium(m,center=mp_center,width=None,height=700,zoom=18)
+			elif(len(selected_row_ids)>0) and ((len(selected_row_ids)>=2)):
+				st_folium(m,width=None,height=700,zoom=16)
+			else:
+				st_folium(m,width=None,height=700)
 		if geom=='point':
 			pass
 
--- search_results_utils.py
+++ search_results_utils.py
@@ -37,44 +37,54 @@ def preprocess_data(gdf):
 
 @st.fragment()
 def render_parcels():
-	with st.spinner("Loading the parcels..."):
-		parcels_gdf = preprocess_data(load_parcels())
+	
+	# row0  = st.columns([1])
+	# map_loc = row0[0].container(border=False,height=600)
+
+
+	
+	parcels_with_geom_gdf = preprocess_data(load_parcels())
+	borders_gdf = load_borders()
+	
+	parcels_gdf = parcels_with_geom_gdf.copy()
+	parcels_gdf = parcels_gdf.drop(columns=['geometry'])
+	# parcels_gdf.loc[:,'selected'] = False
+	# parcels_gdf = parcels_gdf[["selected"]+[c for c in parcels_gdf.columns if c !='selected']]
+	sel_cols_for_map = ['egrid','geometry',
+						'land area',
+						'latest construction year in land',
+						'latest construction period in land',
+						'land plot area ratio',
+						'land number of buildings',
+						'oereb_TypeCode',
+						'oereb_TypeCode_Text',
+						'oereb_TypeCode_Description',
+						'oereb_Gesamthoehe',
+						'oereb_Gebaeudehoehe_Max',
+						'oereb_Firsthoehe_Max',
+						'oereb_Ausnuetzungsziffer_Min',
+						'oereb_Ausnuetzungsziffer_Max',
+						'oereb_Baumassenziffer_Min',
+						'oereb_Baumassenziffer_Max',
+						'oereb_Ueberbauungsziffer_Min',
+						'oereb_Ueberbauungsziffer_Max',
+						'oereb_Vollgeschosse_Max',
+						'oereb_Dachgeschosse_Max',
+						'oereb_Untergeschosse_Max',
+						'oereb_Freiflaechenziffer_Min',
+						'oereb_Wohnanteil_Min',
+						'oereb_Wohnanteil_Max',
+						'oereb_Gewerbeanteil_Min',
+						'oereb_Gewerbeanteil_Max']
+	sel_cols_for_map = [c for c in sel_cols_for_map if c in parcels_with_geom_gdf.columns]
+	render_land_map(parcels_with_geom_gdf,borders_gdf,sel_cols_for_map,geom='polygon')		
 		
-		borders_gdf = load_borders()
-		sel_cols_for_map = ['egrid','geometry',
-							'land area',
-							'latest construction year in land',
-							'latest construction period in land',
-							'land plot area ratio',
-							'land number of buildings',
-							'oereb_TypeCode',
-							'oereb_TypeCode_Text',
-							'oereb_TypeCode_Description',
-							'oereb_Gesamthoehe',
-							'oereb_Gebaeudehoehe_Max',
-							'oereb_Firsthoehe_Max',
-							'oereb_Ausnuetzungsziffer_Min',
-							'oereb_Ausnuetzungsziffer_Max',
-							'oereb_Baumassenziffer_Min',
-							'oereb_Baumassenziffer_Max',
-							'oereb_Ueberbauungsziffer_Min',
-							'oereb_Ueberbauungsziffer_Max',
-							'oereb_Vollgeschosse_Max',
-							'oereb_Dachgeschosse_Max',
-							'oereb_Untergeschosse_Max',
-							'oereb_Freiflaechenziffer_Min',
-							'oereb_Wohnanteil_Min',
-							'oereb_Wohnanteil_Max',
-							'oereb_Gewerbeanteil_Min',
-							'oereb_Gewerbeanteil_Max']
-		sel_cols_for_map = [c for c in sel_cols_for_map if c in parcels_gdf.columns]
-	with st.spinner("Loading the map..."):
-		render_land_map(parcels_gdf,borders_gdf,sel_cols_for_map,geom='polygon')
-		column_config = {}
-		column_config['building zone extract pdf'] = st.column_config.LinkColumn("ÖREB extract", display_text="ÖREB extract")
-		
-		st.data_editor(
-			parcels_gdf.drop(columns=['geometry']),
-			column_config=column_config,
-			)
+	column_config = {}
+	column_config['building zone extract pdf'] = st.column_config.LinkColumn("ÖREB extract", display_text="ÖREB extract")
+	st.data_editor(
+		parcels_gdf,
+		key='parcel_table',
+		column_config=column_config,
+		)
+	# st.write(st.session_state)
 	
